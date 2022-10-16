75a7bfcf3bfa7ab4b4a5569b0353debcb1243eed
cecilcodespython <107161623+cecilcodespython@users.noreply.github.com>
2022-10-16 21:31:16 +0100

    first commit

---
+++ students/models.py
@@ -0,0 +1,15 @@
+from unittest.util import _MAX_LENGTH
+from django.db import models
+
+# Create your models here.
+
+class Student(models.Model):
+    student_number = models.PositiveIntegerField()
+    first_name = models.CharField(max_length=50)
+    last_name =models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+    course = models.CharField(max_length=50)
+    cwa = models.FloatField()
+
+    def __str__(self) -> str:
+        return f'Student: {self.first_name} {self.last_name }'
